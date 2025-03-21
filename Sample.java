7c957b7747f7a4817a32187ca1cd5cb326678daa
jayanthanish <75733069+jayanthanish@users.noreply.github.com>
2025-03-21 11:57:33 -0400

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,9 @@
+import com.google.common.collect.Lists;
+
+public class Sample {
+  public void test() {
+    // Using Guava's API call – for example, Lists.newArrayList
+    List<String> list = Lists.newArrayList("a", "b", "c");
+    System.out.println(list);
+  }
+}
