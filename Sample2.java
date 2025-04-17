4435bb19f040e1bce3466c1bded08a8e009c177d
jayanthanish <75733069+jayanthanish@users.noreply.github.com>
2025-04-16 20:41:58 -0400

    Update Sample2.java

--- Sample2.java
+++ Sample2.java
@@ -7,12 +7,7 @@ public class SampleGuava {
         
         // — Affected code: Guava’s Charsets.UTF_8 is deprecated
         byte[] bytesDeprecated = text.getBytes(Charsets.UTF_8);
-        String sDeprecated    = new String(bytesDeprecated, Charsets.UTF_8);
         System.out.println("Deprecated → " + sDeprecated);
-        
-        // — Modern replacement:
-        byte[] bytesModern = text.getBytes(StandardCharsets.UTF_8);
-        String sModern     = new String(bytesModern, StandardCharsets.UTF_8);
-        System.out.println("Modern    → " + sModern);
+
     }
 }
